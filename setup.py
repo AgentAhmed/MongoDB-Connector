5d45317ce49fd15dc8eb53aa59a916c4a22a3cee
Ahmed <ahmed.mca18@gmail.com>
2025-03-06 00:50:28 +0530

    updated code and fixed issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 
-def get_requirement(file_path:str)->List[str]:
+'''def get_requiremet(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as f:
         requirements=f.readlines()
@@ -11,16 +11,16 @@ def get_requirement(file_path:str)->List[str]:
         
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    return requirements
+    return requirements'''
 
    
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
-REPO_NAME = "MongoDB-Connector"
-PKG_NAME= "databaseautomation"
+__version__ = "0.0.6"
+REPO_NAME = "mongodb_connector"
+PKG_NAME= "MongoConnect"
 AUTHOR_USER_NAME = "AgentAhmed"
 AUTHOR_EMAIL = "ahmed.mca18@gmail.com"
 
@@ -38,7 +38,6 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requirement("requirements_dev.txt"),
     
     
 )
